4b820fe1e258d05b00f102b6f7fbabd88e1e1ede
Ashish yaduveer <ashishyaduveer791@gmail.com>
2025-10-05 01:05:25 +0530

    Add files via upload

---
+++ coding challenge#1.js
@@ -0,0 +1,16 @@
+let BMI='BODY MASS INDEX MARKS';//MARKS AND JHONS TRYING TOCOMPARE THERI BMI?
+console.log(BMI);
+
+const Marksmass = 78;
+const  Marksheight = 1.68;
+console.log(Marksmass/Marksheight*2) ;
+let BMIJhons='BODY MASS INDEX Jhons';
+console.log(BMIJhons);
+
+const Jhonsmass = 92;
+const  Jhonsheight = 1.95;
+console.log(Jhonsmass/Jhonsheight*2);
+
+const JHONSBMI = 94.35;
+const MARKSBMI = 92.85;
+console.log(JHONSBMI>MARKSBMI);
